acc160876b45497d40ef2acbdea0d8a3bb3ca2d4
rconjoe <joe@rconjoe.com>
2022-01-10 12:53:42 -0800

    still use proxy. proxy needs to accept hosted domains for cors

--- src/lib/fetchCharacters.js
+++ src/lib/fetchCharacters.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default async function fetchCharacters() {
   return (
-    await axios.get('https://giantbomb.com/api/characters/?api_key=161a437ab0f7b3a41b3859677505baea6a2e9827&format=json')
+    await axios.get('http://proxy.rconjoe.com:12120/https://giantbomb.com/api/characters/?api_key=161a437ab0f7b3a41b3859677505baea6a2e9827&format=json')
     .catch(err => {
       console.error(err)
     })
