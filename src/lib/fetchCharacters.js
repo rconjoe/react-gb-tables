c937b21db5e74226271408bf34165e7d404a255f
rconjoe <joe@rconjoe.com>
2022-01-10 11:55:40 -0800

    just an axios call to fetch initial array of 100 characters

---
+++ src/lib/fetchCharacters.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export default async function fetchCharacters() {
+  return (
+    await axios.get('http://proxy.rconjoe.com:12120/https://giantbomb.com/api/characters/?api_key=161a437ab0f7b3a41b3859677505baea6a2e9827&format=json')
+    .catch(err => {
+      console.err(err)
+    })
+  ).data.results
+}
